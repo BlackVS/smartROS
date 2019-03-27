bdcea00d0d439d98b2d6c75f63054e5b140b6daf
BlackVS <vvs@polesye.com>
2019-03-27 13:36:06 +0200

    doc updated

--- src/test_ban_ip.py
+++ src/test_ban_ip.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# coding: utf-8
 import sys, os, re
 import argparse
 import smartROS
--- src/test_console.py
+++ src/test_console.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     try:
         #router = smartROS.getRouter("No SSL",True); # r/o, Simple conenction without SSL
         #router = smartROS.getRouter("TLS",True); # r/o, TLS without certificate
-        router = smartROS.getRouter("TLS+cert",True); # r/o, TLS with certificate
+        #router = smartROS.getRouter("TLS+cert",True); # r/o, TLS with certificate
+        router = smartROS.getRouter("TLS+cert",False); # r/w, TLS with certificate
     except Exception as inst:
         print("Error {}".format(str(inst)))
     if router:
